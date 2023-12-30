66f02a24c7d00a03ebe1260975d4aa7391dea35c
FaranAiki <abang.faran.aiki@gmail.com>
2023-12-30 16:53:27 +0700

    modified nihwm -v

--- nihwm.c
+++ nihwm.c
@@ -2345,7 +2345,7 @@ main(int argc, char *argv[])
     char *execconfig[] = { "/bin/sh", "-c", configfile, NULL };
 
 	if (argc == 2 && !strcmp("-v", argv[1]))
-		printf("nihwm-"VERSION); /* don't die */
+		printf("nihwm-"VERSION), exit(1); /* don't die */
 	else if (argc == 2 && !strcmp("-h", argv[1]))
 		die("usage: nihwm [-v] or nihwm [-no-startapp] or nihwm [-replace]");
 
